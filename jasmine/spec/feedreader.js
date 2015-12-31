9fdc4ebd0c64bd3bbf75698bafc6520204650d7d
murat <murat.ozgul@sv.cmu.edu>
2015-12-30 22:28:36 -0800

    RSS feeds complete

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,20 +27,30 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('should have valid URLs', function() {
+            allFeeds.forEach(function(feed, index, array){
+                expect(feed.url).toBeTruthy();
+            });
+        });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* Loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('should have valid names', function() {
+            allFeeds.forEach(function(feed, index, array){
+                expect(feed.name).toBeTruthy();
+            });
+        });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Test suite named "The menu" */
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -53,8 +63,11 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          
+    });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -63,10 +76,16 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+    });
+
+    /* Test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+    });
+
 }());
